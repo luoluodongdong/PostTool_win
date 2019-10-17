02acad0b2180877556048e14ebdaf8802130c605
weidong7171 <826424993@qq.com>
2019-10-17 10:42:00 +0800

    v1.0.0 initial version

---
+++ MEStest001/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace MEStest001
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.msgTB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataTB = new System.Windows.Forms.TextBox();
+            this.btn_request = new System.Windows.Forms.Button();
+            this.rb_POST = new System.Windows.Forms.RadioButton();
+            this.rb_GET = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_url = new System.Windows.Forms.TextBox();
+            this.lb_status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // msgTB
+            // 
+            this.msgTB.Location = new System.Drawing.Point(36, 274);
+            this.msgTB.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.msgTB.Multiline = true;
+            this.msgTB.Name = "msgTB";
+            this.msgTB.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.msgTB.Size = new System.Drawing.Size(882, 408);
+            this.msgTB.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(31, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 27);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Data:";
+            // 
+            // dataTB
+            // 
+            this.dataTB.Location = new System.Drawing.Point(93, 84);
+            this.dataTB.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataTB.Multiline = true;
+            this.dataTB.Name = "dataTB";
+            this.dataTB.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.dataTB.Size = new System.Drawing.Size(825, 129);
+            this.dataTB.TabIndex = 6;
+            // 
+            // btn_request
+            // 
+            this.btn_request.Location = new System.Drawing.Point(290, 232);
+            this.btn_request.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_request.Name = "btn_request";
+            this.btn_request.Size = new System.Drawing.Size(132, 34);
+            this.btn_request.TabIndex = 7;
+            this.btn_request.Text = "Request";
+            this.btn_request.UseVisualStyleBackColor = true;
+            this.btn_request.Click += new System.EventHandler(this.btn_request_Click);
+            // 
+            // rb_POST
+            // 
+            this.rb_POST.AutoSize = true;
+            this.rb_POST.Checked = true;
+            this.rb_POST.Location = new System.Drawing.Point(37, 237);
+            this.rb_POST.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rb_POST.Name = "rb_POST";
+            this.rb_POST.Size = new System.Drawing.Size(69, 24);
+            this.rb_POST.TabIndex = 8;
+            this.rb_POST.TabStop = true;
+            this.rb_POST.Text = "POST";
+            this.rb_POST.UseVisualStyleBackColor = true;
+            // 
+            // rb_GET
+            // 
+            this.rb_GET.AutoSize = true;
+            this.rb_GET.Location = new System.Drawing.Point(165, 237);
+            this.rb_GET.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rb_GET.Name = "rb_GET";
+            this.rb_GET.Size = new System.Drawing.Size(58, 24);
+            this.rb_GET.TabIndex = 9;
+            this.rb_GET.Text = "GET";
+            this.rb_GET.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(37, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 25);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "URL:";
+            // 
+            // tb_url
+            // 
+            this.tb_url.Location = new System.Drawing.Point(94, 17);
+            this.tb_url.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tb_url.Multiline = true;
+            this.tb_url.Name = "tb_url";
+            this.tb_url.Size = new System.Drawing.Size(825, 47);
+            this.tb_url.TabIndex = 11;
+            // 
+            // lb_status
+            // 
+            this.lb_status.BackColor = System.Drawing.Color.Aqua;
+            this.lb_status.Location = new System.Drawing.Point(477, 232);
+            this.lb_status.Name = "lb_status";
+            this.lb_status.Size = new System.Drawing.Size(403, 35);
+            this.lb_status.TabIndex = 12;
+            this.lb_status.Text = "Ready";
+            this.lb_status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 693);
+            this.Controls.Add(this.lb_status);
+            this.Controls.Add(this.tb_url);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rb_GET);
+            this.Controls.Add(this.rb_POST);
+            this.Controls.Add(this.btn_request);
+            this.Controls.Add(this.dataTB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.msgTB);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "MEStest";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox msgTB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox dataTB;
+        private System.Windows.Forms.Button btn_request;
+        private System.Windows.Forms.RadioButton rb_POST;
+        private System.Windows.Forms.RadioButton rb_GET;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_url;
+        private System.Windows.Forms.Label lb_status;
+    }
+}
+
---
+++ MEStest001/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.IO;
+using System.Management;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Net;
+using System.Diagnostics;
+
+namespace MEStest001
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tb_url.Text = "http://172.17.32.29:5611/Bobcat/sfc_response.aspx";
+
+            dataTB.Text = "c=QUERY_RECORD_PANEL&panel_sn=123&StationID=qwa&GET_SN=0&cmd=QUERY_PANEL_LOCATION";
+        }
+
+        private string WebRequst(string url, string content,string method,ref string error)
+        {
+            string result = "";
+            try
+            {
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+                req.Timeout = 5000;
+                req.Method = method;
+                req.ContentType = "application/x-www-form-urlencoded";
+
+                #region 添加Post 参数
+                byte[] data = Encoding.UTF8.GetBytes(content);
+                req.ContentLength = data.Length;
+                using (Stream reqStream = req.GetRequestStream())
+                {
+                    reqStream.Write(data, 0, data.Length);
+                    reqStream.Close();
+                }
+                #endregion
+
+                HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+                Stream stream = resp.GetResponseStream();
+                //获取响应内容
+                using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    result = reader.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                error = ex.ToString();
+            }
+            //调试输出
+            Console.WriteLine("Info:POST result:" + result);
+            return result;
+        }
+
+        private void btn_request_Click(object sender, EventArgs e)
+        {
+            lb_status.BackColor = Color.Yellow;
+            lb_status.Text = "Request...";
+
+            msgTB.Text = string.Format("[{0}]:{1}\r\n", DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss:fff"),"Request...");
+
+            string str_URL = tb_url.Text;
+            string str_DATA= dataTB.Text;
+            if (str_URL == "" || str_DATA == "")
+            {
+                MessageBox.Show("URL or DATA empty error!");
+                return;
+            }
+            string method = "POST";
+            if (rb_GET.Checked) method = "GET";
+            string error = "";
+            string mesMsg = WebRequst(str_URL, str_DATA,method,ref error);
+            if(error != "")
+            {
+                lb_status.BackColor = Color.Red;
+                lb_status.Text = "Error.";
+            }
+            else
+            {
+                lb_status.BackColor = Color.Green;
+                lb_status.Text = "Done.";
+            }
+            Console.WriteLine(error+mesMsg );
+            msgTB.Text += string.Format("[{0}]:{1}", DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss:fff"), error + mesMsg); 
+        }
+    }
+}
